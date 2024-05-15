7a55dea07b152612c232cdc263300066dc1fc63d
qalqio <website@qalq.io>
2024-05-15 11:24:59 +0800

    rebuild 1

--- src/data.js
+++ src/data.js
@@ -18,7 +18,7 @@ export { HeroDark as Dark };
  **************************************************************
   Add your GitHub username (string - "YourUsername") below.
 */
-export const githubUsername = "edmacabebe";
+export const githubUsername = "qalqio";
 
 // Navbar Logo image
 export const navLogo = undefined;
